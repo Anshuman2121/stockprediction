7aef081019ad66ee94c4e296e2af6994d507e600
devopsinsiders <anshuman.abhishek@sebx.se>
2023-07-24 01:35:21 +0200

    Added Chart

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 import yfinance as yf
 import pandas as pd
+import plotly.graph_objects as go
+import numpy as np
+from datetime import datetime, timedelta
 from flask_frozen import Freezer 
 import sys
 
@@ -76,20 +79,77 @@ def display_table():
 
     return render_template('table_template.html', table_data=table_data)
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+@app.route('/chart')
+def display_candlestick_chart():
+    symbol = "^NSEI"
+    end_date = datetime.now().strftime("%Y-%m-%d")
+    start_date = (datetime.now() - timedelta(days=5*365)).strftime("%Y-%m-%d")
+    data = yf.download(symbol, start=start_date, end=end_date)
+    data.reset_index(inplace=True)
+    df = pd.DataFrame({
+        'time': data['Date'],
+        'open': data['Open'],
+        'high': data['High'],
+        'low': data['Low'],
+        'close': data['Close'],
+        'volume': data['Volume']
+    })
+    df = df[df['volume'] != 0]
+    df.reset_index(drop=True, inplace=True)
+
+    num_trading_days = len(df)
+    window = int(num_trading_days * 0.05)
+
+    fig = go.Figure(data=[go.Candlestick(x=df['time'],
+                open=df['open'],
+                high=df['high'],
+                low=df['low'],
+                close=df['close'])])
+
+    def calculate_ascending_channel_lines(data, window):
+        highs = data['high'].rolling(window=window, min_periods=1).max()
+        lows = data['low'].rolling(window=window, min_periods=1).min()
+        return highs, lows
+
+    highs, lows = calculate_ascending_channel_lines(df, window)
+    swing_high_indices = highs.dropna().index
+    swing_low_indices = lows.dropna().index
+    slmax, intercmax = np.polyfit(swing_high_indices, highs.dropna(), 1)
+    slmin, intercmin = np.polyfit(swing_low_indices, lows.dropna(), 1)
+
+    num_future_days = 120
+    last_date = pd.to_datetime(df['time'].iloc[-1])
+    future_dates = [last_date + timedelta(days=i) for i in range(1, num_future_days+1)]
+    upper_channel_line_future = slmax * np.arange(num_trading_days, num_trading_days + num_future_days) + intercmax
+    lower_channel_line_future = slmin * np.arange(num_trading_days, num_trading_days + num_future_days) + intercmin
+
+    fig.add_trace(go.Scatter(x=future_dates, y=lower_channel_line_future, mode='lines', line=dict(color='red', dash='dash'), name='Lower Channel Line (Future)'))
+    fig.add_trace(go.Scatter(x=future_dates, y=upper_channel_line_future, mode='lines', line=dict(color='blue', dash='dash'), name='Upper Channel Line (Future)'))
+    fig.add_trace(go.Scatter(x=df['time'], y=slmin*np.arange(num_trading_days) + intercmin, mode='lines', line=dict(color='red'), name='Lower Channel Line'))
+    fig.add_trace(go.Scatter(x=df['time'], y=slmax*np.arange(num_trading_days) + intercmax, mode='lines', line=dict(color='blue'), name='Upper Channel Line'))
+
+    fig.update_layout(
+        # title="Candlestick Chart for ^NSEI",
+        xaxis_title="Date",
+        yaxis_title="Price",
+        margin=dict(l=50, r=50, t=50, b=50),  # Adjust margins for better visualization
+        autosize=True, 
+        # height=800, 
+    )
+
+    chart_div = fig.to_html(full_html=False, include_plotlyjs='cdn')
+    response = app.make_response(render_template('candlestick_chart_template.html', chart_div=chart_div))
+    response.headers['Content-Type'] = 'text/html; charset=utf-8'
+
+    return response
 
 if __name__ == '__main__':
-    freezer = Freezer(app)  # Create a Freezer object
+    freezer = Freezer(app)
 
-    # If you want to freeze your application, run this command:
-    # python app.py freeze
-
-    # If you want to run the development server, use this command:
-    # python app.py runserver
-
-    # The following block should be used only when freezing the application
     if len(sys.argv) > 1 and sys.argv[1] == 'freeze':
-        freezer.freeze()  # Freeze the application into static files
+        freezer.freeze()
     else:
         app.run(debug=True)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
